bc626ca8846a462087a8c40a92e4720c555376e9
Shakil Ahmed <ahmed1shakil@gmail.com>
2024-07-23 17:21:27 +0100

    have made the data dynamic according to what timeframe the user picks

--- script.js
+++ script.js
@@ -6,5 +6,38 @@ fetch('/data.json').then((request) => {
 
     return request.json()
 }).then((data) => {
-    console.log(data)
+
+const dailyBtn = document.querySelector('#daily');
+const weeklyBtn = document.querySelector('#weekly');
+const monthlyBtn = document.querySelector('#monthly');
+
+const currentYears = document.querySelectorAll('.current-year');
+const previousYears = document.querySelectorAll('.previous-year')
+
+
+
+
+dailyBtn.addEventListener('click', () => {
+    for (let i = 0; i < data.length; i++){
+        currentYears[i].innerText = `${data[i].timeframes.daily.current}hrs`;
+         previousYears[i].innerText = `Last week - ${data[i].timeframes.daily.previous}hrs`;
+    }
 })
+
+weeklyBtn.addEventListener('click', () => {
+    for (let i = 0; i < data.length; i++){
+        currentYears[i].innerText = `${data[i].timeframes.weekly.current}hrs`;
+         previousYears[i].innerText = `Last week - ${data[i].timeframes.weekly.previous}hrs`;
+    }
+})
+
+monthlyBtn.addEventListener('click', () => {
+    for (let i = 0; i < data.length; i++){
+        currentYears[i].innerText = `${data[i].timeframes.monthly.current}hrs`;
+         previousYears[i].innerText = `Last week - ${data[i].timeframes.monthly.previous}hrs`;
+    }
+})
+})
+
+
+
